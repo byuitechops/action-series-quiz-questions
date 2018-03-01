d265a3d07372486eeb1fed518de6597e2688b592
Seth Childers <sethchilders92@gmail.com>
2018-03-01 12:03:39 -0700

    Updated template file

--- template-quiz-questions.js
+++ template-quiz-questions.js
@@ -3,7 +3,7 @@ const canvas = require('canvas-wrapper');
 
 /* Actions */
 var actions = [
-    require('../actions/files-delete.js'),
+    // require('./actions/quiz-questions-delete.js'),
 ];
 
 class TechOps {
@@ -16,14 +16,20 @@ class TechOps {
         this.setTitle = setTitle;
         this.getID = getID;
         this.delete = false;
-        this.type = 'File';
+        this.type = 'Quiz Question';
+        this.quiz_id = this.quiz_id;
     }
 }
 
+/********************************************************
+ *  BETA: This API resource is not finalized, and there 
+ * could be breaking changes before its final release. 
+ ********************************************************/
+
 /* Retrieve all items of the type */
 function getItems(course, callback) {
-    /* Get all of the files from Canvas */
-    canvas.getFiles(course.info.canvasOU, (err, items) => {
+    /* Get all of the quiz questions from Canvas */
+    canvas.getQuizQuestions(course.info.canvasOU, (err, items) => {
         if (err) {
             callback(err);
             return;
@@ -38,22 +44,29 @@ function getItems(course, callback) {
 }
 
 /* Build the PUT object for an item */
-function buildPutObj(file) {
+function buildPutObj(question) {
     return {
-        'name': file.display_name,
-        'parent_folder_id': file.parent_folder_id,
-        'on_duplicate': 'rename',
-        /* rename/overwrite - rename adds a qualifier to make the new filename unique, overwrite replaces the existing 
-                                            file with the same name. Without either, uploading a duplicate file will simply fail and we may not know why */
-        'lock_at': file.lock_at,
-        'unlock_at': file.unlock_at,
-        'locked': file.locked,
-        'hidden': file.hidden,
+        'quiz_id': question.quiz_id, // required
+        'id': question.id, // required
+        'question': {
+            'question_name': question.question_name,
+            'question_text': question.question_text,
+            // 'quiz_group_id': question.quiz_group_id,     // not included in get request, fyi
+            'question_type': question.question_type,
+            'position': question.position,
+            'points_possible': question.points_possible,
+            'correct_comments': question.correct_comments,
+            'incorrect_comments': question.incorrect_comments,
+            'neutral_comments': question.neutral_comments,
+            // 'text_after_answers': question.text_after_answers,   // not included in get request, fyi
+            'answers': question.answers,
+        }
     };
 }
 
-function deleteItem(course, file, callback) {
-    canvas.delete(`/api/v1/files/${file.id}`, (err) => {
+/****** BETA: This API endpoint is not finalized, and there could be breaking changes before its final release. - Canvas API Documentation ******/
+function deleteItem(course, question, callback) {
+    canvas.delete(`/api/v1/courses/${course.info.canvasOU}/quizzes/${question.techops.quiz_id}/questions/${question.id}`, (err) => {
         if (err) {
             callback(err);
             return;
@@ -63,13 +76,13 @@ function deleteItem(course, file, callback) {
 }
 
 /* PUT an item back into Canvas with updates */
-function putItem(course, file, callback) {
-    if (file.delete == true) {
-        deleteItem(course, file, callback);
+function putItem(course, question, callback) {
+    if (question.delete == true) {
+        deleteItem(course, question, callback);
         return;
     }
-    var putObj = buildPutObj(file);
-    canvas.put(`/api/v1/files/${file.id}`, putObj, (err, newItem) => {
+    var putObj = buildPutObj(question);
+    canvas.put(`/api/v1/courses/${course.info.canvasOU}/quizzes/${question.techops.quiz_id}/questions/${question.id}`, putObj, (err, newItem) => {
         if (err) {
             callback(err);
             return;
@@ -87,19 +100,19 @@ function setHTML(item, newHTML) {
 }
 
 function getTitle(item) {
-    return item.display_name;
+    return item.question_name;
 }
 
 function setTitle(item, newTitle) {
-    item.display_name = newTitle;
+    item.question_name = newTitle;
 }
 
 function getPosition(item) {
-    return null;
+    return item.position;
 }
 
 function setPosition(item, newPosition) {
-    return null;
+    item.position = newPosition;
 }
 
 function getID(item) {
